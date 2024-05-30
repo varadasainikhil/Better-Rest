9a4869c5a4e7d007741883c7fb68a68d7075f1e3
varadasainikhil <varadasainikhil@gmail.com>
2024-05-30 12:05:39 -0400

    Initial Commit

---
+++ Better Rest/Better_RestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Better_RestApp.swift
+//  Better Rest
+//
+//  Created by Sai Nikhil Varada on 5/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Better_RestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Better Rest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Better Rest
+//
+//  Created by Sai Nikhil Varada on 5/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
